9da1a1a81e3c19b918d277dc056e1fcc07542364
Amit Oren <amoren@redhat.com>
2026-08-05 22:05:44 +0300

    OSAC-3428: standardize copyright headers across adapters package
    Signed-off-by: Amit Oren <amoren@redhat.com>

--- osac-metering/adapters/order_test.go
+++ osac-metering/adapters/order_test.go
@@ -1,3 +1,12 @@
+/*
+Copyright (c) 2026 Red Hat, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+in compliance with the License. You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+*/
+
 package adapters
 
 import (
